9e0d10e0f4b7b044d31fde5b23548837efb07bd6
gabri <gabri@DESKTOP-IK5QGDQ>
2022-01-07 12:13:03 -0300

    Agregar archivos de proyecto.

---
+++ operaciones_con_numeros/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operaciones_con_numeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 50;
+            int b = 40;
+            double c = 20.5;
+
+            int suma = a + b;
+            int resta = a - b;
+            int multiplicacion = a * b;
+            int division = a / b;
+            double sumadoble = c + b;
+
+            Console.WriteLine("Suma: " + suma); 
+           Console.WriteLine("Resta: "+resta);
+           Console.WriteLine("Multiplicacion: "+multiplicacion);
+           Console.WriteLine("Division: "+division);
+           Console.WriteLine("Suma Double: "+ sumadoble);
+
+            Console.ReadLine();
+
+        }
+    }
+}
